6f288097fc8fd0649523ecb2dc93009cc3658f24
Appci11 <61198490+Appci11@users.noreply.github.com>
2022-10-13 15:44:26 +0300

    added bUnit  test

---
+++ UnitTests/CounterShould.cs
@@ -0,0 +1,33 @@
+﻿using Xunit;
+using Bunit;
+using OPP_Projektas.Client.Pages;
+
+namespace UnitTests
+{
+    public class CounterShould : TestContext
+    {
+        [Fact]
+        public void RenderCorrectlyWithInitialZero()
+        {
+            IRenderedComponent<CounterComponent>? cut = RenderComponent<CounterComponent>();
+
+            // You can use Assert.Equal, but this is way too sensitive about whitespace...
+            //     Assert.Equal(@"
+            //<h1>Counter</h1>
+            //<p>Current count: 0</p>
+            //<button class=""btn btn-primary"" >
+            //Click me
+            //</button>
+            //", cut.Markup);
+
+            // Better to use semantic comparison with MarkupMatches
+            cut.MarkupMatches(@"
+               <h1>Counter</h1>
+               <p role=""status"">Current count: 0</p>
+               <button class=""btn btn-primary"" >
+               Click me
+               </button>
+           ");
+        }
+    }
+}

6b62b8aa1da227343e2e8a5222bba492059693c3
Appci11 <61198490+Appci11@users.noreply.github.com>
2022-11-02 17:01:49 +0200

    chat now saves and uses chat messages in server

--- OPP_Projektas/Server/GameHubs/ChatHub.cs
+++ OPP_Projektas/Server/GameHubs/ChatHub.cs
@@ -1,29 +1,32 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using OPP_Projektas.Shared.Models.Chat;
 
 namespace OPP_Projektas.Server.GameHubs
 {
     public class ChatHub : Hub
     {
         private static Dictionary<string, string> Users = new Dictionary<string, string>();
+        private static List<Message> Messages = new List<Message>();
 
         public override async Task OnConnectedAsync()
         {
             string username = Context.GetHttpContext().Request.Query["username"];
             Users.Add(Context.ConnectionId, username);
             //await AddMessageToChat(string.Empty, "User connected!");
-            await AddMessageToChat(string.Empty, $"{username} connected!");
+            await AddMessageToChat(username, "connected!");
             await base.OnConnectedAsync();
         }
 
         public override async Task OnDisconnectedAsync(Exception? exception)
         {
             string username = Users.FirstOrDefault(u => u.Key == Context.ConnectionId).Value;
-            await AddMessageToChat(string.Empty, $"{username} left!");
+            await AddMessageToChat(username, "left!");
         }
 
         public async Task AddMessageToChat(string user, string message)
         {
-            await Clients.All.SendAsync("GetThatMessageDude", user, message);
+            Messages.Add(new Message(user, message));
+            await Clients.All.SendAsync("ReceiveMessages", Messages);
         }
     }
 }
---
+++ OPP_Projektas/Shared/Models/Chat/Message.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OPP_Projektas.Shared.Models.Chat
+{
+    public class Message
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Msg { get; set; } = string.Empty;
+
+        public Message()
+        {
+
+        }
+        public Message(string user, string msg)
+        {
+            Username = user;
+            Msg = msg;
+        }
+    }
+}

2b3d094055b9f1f322abf3224b96c3390098bf3a
tomassamot <tomas.matijoska@gmail.com>
2022-11-03 21:54:33 +0200

    Bridge pattern implementation

--- OPP_Projektas/Server/Models/Slots/ISlotStrategy.cs
+++ OPP_Projektas/Server/Models/Slots/ISlotStrategy.cs
@@ -1,9 +1,10 @@
 ﻿using OPP_Projektas.Shared.Models.Enums.Slots;
 using OPP_Projektas.Shared.Models.Slots.SlotSymbols;
+using OPP_Projektas.Shared.Models.Slots.SymbolTiers;
 
 namespace OPP_Projektas.Server.Models.Slots;
 
 public interface ISlotStrategy
 {
-    public (int payout, List<SymbolTier> result) Play(int betAmount, bool isAlternativeStyle);
+    public (int payout, List<ISymbolTier> result) Play(int betAmount, bool isAlternativeStyle);
 }
--- OPP_Projektas/Shared/Models/Slots/SlotSymbols/Rollers/PictureSlotRoller.cs
+++ OPP_Projektas/Shared/Models/Slots/SlotSymbols/Rollers/PictureSlotRoller.cs
@@ -1,11 +1,28 @@
-﻿using OPP_Projektas.Shared.Models.Enums.Slots;
+﻿using OPP_Projektas.Shared.Models.Slots.SymbolTiers;
 
 namespace OPP_Projektas.Shared.Models.Slots.SlotSymbols.Rollers;
 
 public class PictureSlotRoller : SlotRoller
 {
-    public override ISlotSymbol CreateSymbol(SymbolTier symbolTier)
+    public override ISlotSymbol CreateSymbol(int tier)
     {
-        return new PictureSymbol(symbolTier);
+        switch (tier)
+        {
+            case 1:
+                return new PictureSymbol(new TierFirst());
+            case 2:
+                return new PictureSymbol(new TierSecond());
+            case 3:
+                return new PictureSymbol(new TierThird());
+            case 4:
+                return new PictureSymbol(new TierFourth());
+            case 5:
+                return new PictureSymbol(new TierFifth());
+        }
+        return null;
+    }
+    public override ISlotSymbol CreateSymbol(ISymbolTier tier)
+    {
+        return new PictureSymbol(tier);
     }
 }

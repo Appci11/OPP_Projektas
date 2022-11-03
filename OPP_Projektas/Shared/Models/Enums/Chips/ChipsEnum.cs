c3054138f09016cee8ef8d569428243fe4c8dbcf
Appci11 <61198490+Appci11@users.noreply.github.com>
2022-11-03 13:51:42 +0200

    added "show chips" to roulette

---
+++ OPP_Projektas/Shared/Models/Enums/Chips/ChipsEnum.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OPP_Projektas.Shared.Models.Enums.Chips
+{
+    public enum ChipsEnum
+    {
+        White = 1,
+        Red = 5,
+        Blue = 10,
+        Green = 25,
+        Orange = 50,
+        Black = 100
+    }
+}

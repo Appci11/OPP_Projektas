5d3722a76b1f9ce1c5362c248a90959499b5b458
Dominykas Ivanovas <dominykas.ivanovass@gmail.com>
2022-11-04 09:18:04 +0200

    Re-add basic observer implementation

---
+++ OPP_Projektas/Shared/Models/Observer/IObserver.cs
@@ -0,0 +1,47 @@
+﻿namespace OPP_Projektas.Shared.Models.Observer;
+
+public interface IObserver
+{
+    void Update(ISubject subject);
+}
+
+public interface ISubject
+{
+    void Attach(IObserver observer);
+    void Detach(IObserver observer);
+    void Notify();
+}
+
+public class Subject : ISubject
+{
+    public string PropertyName { get; set; }
+    private List<IObserver> _observers = new List<IObserver>();
+    
+    public void Attach(IObserver observer)
+    {
+        Console.WriteLine("Subject: Attached observer");
+        _observers.Add(observer);
+    }
+
+    public void Detach(IObserver observer)
+    {
+        _observers.Remove(observer);
+        Console.WriteLine("Subject: Detached observer");
+    }
+
+    public void Notify()
+    {
+        Console.WriteLine("Notifying all observers");
+        foreach (var observer in _observers)
+        {
+            observer.Update(this);
+        }
+    }
+
+    public void OnPropertyChanged(string propertyName)
+    {
+        Console.WriteLine($"Subject: {propertyName} changed");
+        PropertyName = propertyName;
+        Notify();
+    }
+}

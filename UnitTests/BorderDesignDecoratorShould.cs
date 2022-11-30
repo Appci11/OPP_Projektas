7ce8126f4ed4318ba0e9d6d406186c6af3e27290
Tomas Matijoška <tomas.matijoska@gmail.com>
2022-11-30 19:07:40 +0200

    BorderDesignDecorator and ISlotSymbol tests

---
+++ UnitTests/BorderDesignDecoratorShould.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OPP_Projektas.Shared.Models.BorderDecorator;
+using Xunit.Sdk;
+
+namespace UnitTests
+{
+    public class BorderDesignDecoratorTests
+    {
+        BorderDesign borderDesign;
+
+        [Fact]
+        public void DecorateBordersWithHalloweenThemeAnd20pxWidth()
+        {
+            borderDesign = new BorderWidth("20px");
+            var borderDecorator = new HalloweenDesignDecorator(borderDesign);
+
+            string result = borderDecorator.Decorate("");
+
+            Assert.Equal("border:dotted;border-color:darkorange;border-width:20px;", result);
+        }
+        [Fact]
+        public void DecorateBordersWithChristmasThemeAnd20pxWidth()
+        {
+            borderDesign = new BorderWidth("20px");
+            var borderDecorator = new ChristmasDesignDecorator(borderDesign);
+
+            string result = borderDecorator.Decorate("");
+
+            Assert.Equal("border:double;border-color:red;border-width:20px;", result);
+        }
+        [Fact]
+        public void DecorateBordersWithEasterThemeAnd20pxWidth()
+        {
+            borderDesign = new BorderWidth("20px");
+            var borderDecorator = new EasterDesignDecorator(borderDesign);
+
+            string result = borderDecorator.Decorate("");
+
+            Assert.Equal("border:dotted;border-color:aqua;border-width:20px;", result);
+        }
+        [Fact]
+        public void DecorateBordersWithEasterThemeAndDecimalWidth()
+        {
+            borderDesign = new BorderWidth("20.5px");
+            var borderDecorator = new EasterDesignDecorator(borderDesign);
+
+            string result = borderDecorator.Decorate("");
+
+            Assert.Equal("border:dotted;border-color:aqua;border-width:20.5px;", result);
+        }
+    }
+}
---
+++ UnitTests/ISlotSymbolShould.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OPP_Projektas.Shared.Models.Slots.SlotSymbols;
+using OPP_Projektas.Shared.Models.Slots.SymbolTiers;
+using Xunit.Sdk;
+
+namespace UnitTests
+{
+    public class ISlotSymbolShould
+    {
+        ISlotSymbol slotSymbol;
+
+        [Fact]
+        public void MakeShallowCloneOfColorSymbolAndChangeValue()
+        {
+            slotSymbol = new ColorSymbol(new TierFirst());
+            ISlotSymbol shallowClone = slotSymbol.ShallowClone();
+            ISymbolTier tierSecond = new TierSecond();
+
+            shallowClone.SymbolTier = tierSecond;
+
+            Assert.Equal(shallowClone.SymbolTier,slotSymbol.SymbolTier);
+        }
+        [Fact]
+        public void MakeShallowCloneOfColorSymbolAndCompareHashes()
+        {
+            slotSymbol = new ColorSymbol(new TierFirst());
+            ISlotSymbol shallowClone = slotSymbol.ShallowClone();
+
+            var originalHash = slotSymbol.GetHashCode();
+            var cloneHash = shallowClone.GetHashCode();
+
+            Assert.Equal(originalHash,cloneHash);
+        }
+        [Fact]
+        public void MakeDeepCloneOfColorSymbolAndCompareHashes()
+        {
+            slotSymbol = new ColorSymbol(new TierFirst());
+            ISlotSymbol deepClone = slotSymbol.DeepClone();
+
+            var originalHash = slotSymbol.GetHashCode();
+            var deepCloneHash = deepClone.GetHashCode();
+
+            Assert.False(originalHash==deepCloneHash);
+        }
+        [Fact]
+        public void GetColorSymbolTierFirstRender()
+        {
+            slotSymbol = new ColorSymbol(new TierFirst());
+
+            var result = slotSymbol.Render();
+
+            Assert.Equal("background-color: #0000FF; width: 100px; height: 100px; margin: 10px;", result);
+        }
+
+        [Fact]
+        public void MakeShallowCloneOfPictureSymbolAndChangeValue()
+        {
+            slotSymbol = new PictureSymbol(new TierFirst());
+            ISlotSymbol shallowClone = slotSymbol.ShallowClone();
+            ISymbolTier tierSecond = new TierSecond();
+
+            shallowClone.SymbolTier = tierSecond;
+
+            Assert.Equal(shallowClone.SymbolTier, slotSymbol.SymbolTier);
+        }
+        [Fact]
+        public void MakeShallowCloneOfPictureSymbolAndCompareHashes()
+        {
+            slotSymbol = new PictureSymbol(new TierFirst());
+            ISlotSymbol shallowClone = slotSymbol.ShallowClone();
+
+            var originalHash = slotSymbol.GetHashCode();
+            var cloneHash = shallowClone.GetHashCode();
+
+            Assert.Equal(originalHash,cloneHash);
+        }
+        [Fact]
+        public void MakeDeepCloneOfPictureSymbolAndCompareHashes()
+        {
+            slotSymbol = new PictureSymbol(new TierFirst());
+            ISlotSymbol deepClone = slotSymbol.DeepClone();
+
+            var originalHash = slotSymbol.GetHashCode();
+            var deepCloneHash = deepClone.GetHashCode();
+
+            Assert.False(originalHash==deepCloneHash);
+        }
+        [Fact]
+        public void GetPictureSymbolTierFirstRender()
+        {
+            slotSymbol = new PictureSymbol(new TierFirst());
+
+            var result = slotSymbol.Render();
+
+            Assert.Equal("background-color: #fff; background-image: url('assets/1-first.svg'); width: 100px; height: 100px; margin: 10px;", result);
+        }
+    }
+}

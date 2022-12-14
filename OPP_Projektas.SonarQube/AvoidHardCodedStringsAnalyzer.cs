bcb873a5b86904e1a0f9a87d77168b4aea6990fd
lt0dvvs <lt0dvvs@festo.net>
2022-12-14 22:17:16 +0200

    Add custom rule (not tested)

---
+++ OPP_Projektas.SonarQube/AvoidHardCodedStringsAnalyzer.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Immutable;
+using System.Text.RegularExpressions;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace OPP_Projektas.SonarQube;
+
+[DiagnosticAnalyzer(LanguageNames.CSharp)]
+public class AvoidHardCodedStringsAnalyzer : DiagnosticAnalyzer
+{
+    public const string DiagnosticId = "AvoidHardCodedStrings01";
+
+    private static readonly DiagnosticDescriptor Rule = new(
+        DiagnosticId,
+        "Avoid hardcoded strings",
+        "Avoid using hardcoded strings in your code. Use string literals instead",
+        "Code smell",
+        DiagnosticSeverity.Warning,
+        true);
+        
+    public override void Initialize(AnalysisContext context)
+    {
+        context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+        context.EnableConcurrentExecution();
+        
+        context.RegisterSyntaxNodeAction(
+            c =>
+            {
+                var stringLiteral = (LiteralExpressionSyntax) c.Node;
+                var stringValue = stringLiteral.Token.ValueText;
+
+                if (stringValue.Length >= 3 &&
+                    !Regex.IsMatch(stringValue, "/.*\"(.*?)\"/") &&
+                    !stringValue.StartsWith("Resources.", StringComparison.Ordinal))
+                {
+                    c.ReportDiagnostic(Diagnostic.Create(Rule, stringLiteral.GetLocation(), stringValue));
+                }
+            }, SyntaxKind.StringLiteralExpression);
+    }
+
+    public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
+}
